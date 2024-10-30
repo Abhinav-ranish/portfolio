812817c8e6803e641370e6a3413f27b72e6434fa
Abhinav <abhinav.ranish@outlook.com>
2024-10-30 01:38:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
